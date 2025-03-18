3a7c0d54f25549be128ed4242c0a5adceeb28f7b
PabloTsvetkov <pcvetkov53@gmail.com>
2025-03-18 16:40:58 +0300

    change link

--- frontend/src/LINK.js
+++ frontend/src/LINK.js
@@ -1,4 +1,4 @@
-// const LINK = "https://smartlex.onrender.com";
-const LINK = "http://127.0.0.1:5000";
+const LINK = "https://smartlex.onrender.com";
+// const LINK = "http://127.0.0.1:5000";
 
 export default LINK;
